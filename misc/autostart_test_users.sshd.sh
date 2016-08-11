d753a48f365fa6eda432a174c8c25f01a8483234
Tim Baverstock <tim.baverstock@corp.badoo.com>
2016-08-11 12:03:41 +0100

    Fixes for sshd config

--- misc/autostart_test_users.sshd.sh
+++ misc/autostart_test_users.sshd.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Tweak sshd_config's port to match ordinality of .parallel_calabash.autostart
+# Run with port configured by setup_ios_host reported in .port file. (Originally tweaked by this script)
 echo Home: $HOME
 echo Config: ${0%/*}/../.parallel_calabash.autostart
 date
 date 1>&2
 
-USERS=$( ruby -e "config = eval File.read('${0%/*}/../.parallel_calabash.autostart'); puts config[:USERS]" )
-echo Users: $USERS
-echo Me: $( whoami )
-SSH_PORT=$( ruby -e "config = eval File.read('${0%/*}/../.parallel_calabash.autostart'); puts((config[:USERS].map{|w|w.split('@')[0]}.index(%x(whoami).chomp)||-1)+2201)" )
-echo Starting on port $SSH_PORT
+SSHD_CONFIG=$HOME/ssh/sshd_config
+SSH_PORT=$( perl -nle '/Port ([0-9][0-9]*)/ && print $1' $SSHD_CONFIG )
+echo Found $SSH_PORT in $SSHD_CONFIG
 echo $SSH_PORT >$HOME/autostart_test_users.sshd.port
-sed -E -i~ 's/^#?Port.*/Port '$SSH_PORT'/' $HOME/ssh/sshd_config
 
 # Run sshd as non-daemon
 
-/usr/sbin/sshd -D -f $HOME/ssh/sshd_config
+/usr/sbin/sshd -D -f $SSHD_CONFIG
